7668a1871ba326ecf95e1a540166972aea56ba17
Shamika Redkar <shamikaredkar91@gmail.com>
2024-01-17 18:53:20 -0500

    Populated UI elements for button and button session

--- app.py
+++ app.py
@@ -9,6 +9,34 @@ from langchain_experimental.agents import create_pandas_dataframe_agent
 from dotenv import load_dotenv, find_dotenv
 
 #Main
-st.title('AI Assitant for Data Science')
-st.header('Exploratory Data Analaysis')
-st.subheader('Solution Analysis')
+st.title('DataWiz: AI Assitant for Data Science')
+
+st.write('''Hello there, data enthusiast! 
+         👋 I'm your dedicated AI Assistant for Data Science, here to help you navigate the exciting world of data analysis, machine learning, and much more. 
+         Whether you're a seasoned data scientist, a student diving into the field, or just curious about the power of data, I'm here to assist.''')
+with st.sidebar:
+    #** FOR BOLD
+    st.write('**Welcome to the beginning of your data science journey.**')
+    #* FOR 
+    st.caption('''
+        Every meaningful project starts with data, and yours is no exception. 
+        Let's kickstart our collaboration by uploading a CSV file. 
+        Once your data is in our hands, we'll dive deep into its structure and stories, turning numbers and columns into insights and strategies. 
+        Together, we'll frame your business challenges through the lens of data and apply cutting-edge machine learning models to uncover solutions. 
+        Ready to join forces and explore the possibilities hidden in your data?
+    ''')
+    st.divider()
+    
+#Initialize the ket in the session state
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = {1:False}
+    
+#Function to update the value in the session state
+def clicked(button):
+    st.session_state.clicked[button] = True
+st.button("Let's dive in!", on_click = clicked, args=[])
+if st.session_state.clicked[1]:
+    st.header('Your Smart Data Science Companion')
+    st.subheader('Unleashing the Power of Data, Together!')
+    user_csv = st.file_uploader('Upload your file here', type='csv')
+    
